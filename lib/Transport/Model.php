3d7bd72d34d91f1a1cf984cf188f59103c300bf8
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-29 15:51:40 -0400

    first try at relationship helpers for Model

--- lib/Transport/Model.php
+++ lib/Transport/Model.php
@@ -3,5 +3,18 @@ namespace Transport;
 
 abstract class Model
 {
-	
+	protected function has_one($class,$column)
+	{
+		return $class::find($this->$column);
+	}
+
+	protected function has_many($class,$column)
+	{
+		return $class::where('id',$this->$column);
+	}
+
+	protected function belongs_to($class,$foreign_key)
+	{
+		return $class::where($foreign_key,$this->id())->first();
+	}
 }
