46251c2ab08ada9be00d32ddc2eb1d387bdf1a91
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-30 14:06:30 -0400

    model relationships should only return an embedded data type if it exists. Otherwise lets return a model collection

--- lib/Transport/Model.php
+++ lib/Transport/Model.php
@@ -5,16 +5,23 @@ abstract class Model
 {
 	protected function has_one($class,$column)
 	{
-		return $class::find($this->$column);
+		return $this->data_or_relationship($class,$column,'id');
 	}
 
 	protected function has_many($class,$column)
 	{
-		return $class::where('id',$this->$column);
+		return $this->data_or_relationship($class,$column,'id',true);
 	}
 
 	protected function belongs_to($class,$foreign_key)
 	{
-		return $class::where($foreign_key,$this->id())->first();
+		return $this->data_or_relationship($class,'id',$foreign_key);
+	}
+
+	protected function data_or_relationship($class, $column, $key, $many = false)
+	{
+		if(is_array($this->$column) || is_object($this->$column)) return $this->$column;
+		$result = $class::where($key,$this->$column);
+		return $many? $result : $result->first();
 	}
 }
