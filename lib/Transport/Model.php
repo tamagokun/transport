6c749bf6d6d29e944193a5530f83bee86ff9adf9
Mike Kruk <mike.kruk@mslgroup.com>
2012-05-31 17:09:58 -0400

    added all core model methods. need to create transport pass-in

--- lib/Transport/Model.php
+++ lib/Transport/Model.php
@@ -9,11 +9,63 @@ abstract class Model
 	protected $attributes = array();
 	protected $dirty = array();
 
+	/* Static */
+	public static function all()
+	{
+		$class = get_called_class();
+		return $class::build_query(func_get_args());
+	}
+
+	public static function any_in($prop,$values=array())
+	{
+		$class = get_called_class();
+		return $class::where($prop,array('in'=>$values));
+	}
+
+	public static function find($id)
+	{
+		$class = get_called_class();
+		return is_array($id)? $class::any_in('id',$id) : $class::where('id=?',$id)->first();
+	}
+
+	public static function first() { return static::all(func_get_args())->first(); }
+	public static function last() { return static::all(func_get_args())->last(); }
+
+	public static function where($prop,$value=null)
+	{
+		$class = get_called_class();
+		$params = array();
+		if(is_array($prop))
+		{
+			foreach($prop as $attribute=>$value)
+			  $params[] = transport()->where($prop,$value);
+		}else $params = transport()->where($prop,$value);
+		return $class::query($params);
+	}
+
+	public static function query($params = array())
+	{
+		return new ModelCollection(get_called_class(),$params);
+	}
+
+	protected static function build_query($args = array())
+	{
+		$class = get_called_class();
+		if(count($args) == 2) return $class::where($args[0],$args[1]);
+		if(count($args) == 1 && !empty($args[0])) return $class::where($args[0][0],$args[0][1]);
+		return $class::query();
+	}
+
 	/* Public */
 	public function __construct($attrs = array())
 	{
 		if($attrs) foreach($attrs as $key=>$value) $this->attributes[$key] = $value;
 		$this->id = $this->id();
+	}
+
+	public function attr_properties()
+	{
+		return $this->attributes;
 	}
 
 	public function collection()
@@ -22,9 +74,92 @@ abstract class Model
 		return strtolower(preg_replace('/(?<=[a-z])([A-Z])/', '_$1', get_class($this)));
 	}
 
+	public function id()
+	{
+		$id = transport()->id_attribute;
+		$value = isset($this->attributes['id'])? $this->attributes['id'] : null;
+		if(isset($this->attributes[$id])) $valie = $this->attributes[$id];
+		return !is_string($value) && !is_null($value)? $value->__toString() : $value;
+	}
+
 	public function is_dirty($key)
 	{
 		return isset($this->dirty[$key]);
+	}
+
+	public function properties()
+	{
+		return array();
+	}
+
+	public function save()
+	{
+		if(!$this->before_save()) return false;
+		if($this->id())
+		  if(!$this->update()) return false;
+		else if(!$this->insert()) return false;
+		if(!$this->after_save()) return false;
+		return true;
+	}
+
+	public function destroy()
+	{
+		if(!$this->before_destroy()) return false;
+		if(!transport()->destroy($this)) return false;
+		if(!$this->after_destroy())
+		{
+			$this->save();
+			return false;
+		}
+		return true;
+	}
+
+	public function validate()
+	{
+		if(!$this->before_validate()) return false;
+		if(!$this->validation()) return false;
+		if(!$this->after_validate()) return false;
+		return true;
+	}
+
+	protected function before_save() { return true; }
+	protected function before_insert() { return true; }
+	protected function before_update() { return true; }
+	protected function before_destroy() { return true; }
+	protected function before_validate() { return true; }
+	protected function after_save() { return true; }
+	protected function after_insert() { return true; }
+	protected function after_update() { return true; }
+	protected function after_destroy() { return true; }
+	protected function after_validate() { return true; }
+
+	protected function update()
+	{
+		if(!$this->before_update()) return false;
+		if(!transport()->update($this)) return false;
+		if(!$this->after_update())
+		{
+			//TODO : rollback
+			return false;
+		}
+		return true;
+	}
+
+	protected function insert()
+	{
+		if(!$this->before_insert()) return false;
+		if(!transport()->insert($this)) return false;
+		if(!$this->after_insert())
+		{
+			$this->destroy();
+			return false;
+		}
+		return true;
+	}
+
+	protected function validation()
+	{
+		return true;
 	}
 
 	/* Protected */
